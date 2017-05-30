a6b500d3dbd0b408ea4ca44d9d80518ab93ab1ab
Marc Dostie <marc.dostie@gmail.com>
2017-05-30 18:33:44 -0400

    added lib and test

---
+++ test/acl_test.js
@@ -0,0 +1,21 @@
+var acl = require("../lib/myacl")
+
+exports["setUp"] = function(callback) {
+    // reset method to allow pristine unit test function runs
+    
+    callback();
+};
+
+exports["Initialization Test"] = function(test) {
+
+
+    test.done();
+};
+
+exports["Member access to Care Manager"] = function(test) {
+
+
+    test.done();
+};
+
+
